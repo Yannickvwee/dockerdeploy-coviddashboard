c2a6498984d8a855f1fe26b2cbfb2a04a4ca9c1f
Yannickvwee <yannickvanwee@gmail.com>
2020-10-22 01:50:10 +0200

    wrong token oops

--- swarmmanager.sh
+++ swarmmanager.sh
@@ -7,4 +7,4 @@ git clone https://github.com/broadinstitute/covid19-testing.git ./nginx/master
 git clone https://github.com/looking4ward/nhs-cac-docker-dotnetwebapp.git ./netapp/master
 cp ./Dockerfile.dockerfile ./nginx/Dockerfile
 docker swarm init
-docker swarm join-token manager -q > $TOKENPATH
+docker swarm join-token worker -q > $TOKENPATH
