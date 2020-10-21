b9ac159134c3af0b96d61e984a16388c16107d51
Yannickvwee <yannickvanwee@gmail.com>
2020-10-21 16:51:41 +0200

    it works i guess

--- swarmmanager.sh
+++ swarmmanager.sh
@@ -5,6 +5,6 @@ TOKENPATH=$1
 
 git clone https://github.com/broadinstitute/covid19-testing.git ./nginx/master
 git clone https://github.com/looking4ward/nhs-cac-docker-dotnetwebapp.git ./netapp/master
-
+cp ./Dockerfile.dockerfile ./nginx/Dockerfile
 docker swarm init
 docker swarm join-token manager -q > $TOKENPATH
