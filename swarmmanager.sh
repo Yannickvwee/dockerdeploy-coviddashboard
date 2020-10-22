3da2e412c7acac92c1846a0e5a530d4e3fe699d2
Yannickvwee <yannickvanwee@gmail.com>
2020-10-22 02:41:51 +0200

    new compose

--- swarmmanager.sh
+++ swarmmanager.sh
@@ -5,6 +5,6 @@ TOKENPATH=$1
 
 git clone https://github.com/broadinstitute/covid19-testing.git ./nginx/master
 git clone https://github.com/looking4ward/nhs-cac-docker-dotnetwebapp.git ./netapp/master
-cp ./Dockerfile.dockerfile ./nginx/Dockerfile
+mv ./Dockerfile.dockerfile ./nginx/Dockerfile
 docker swarm init
 docker swarm join-token worker -q > $TOKENPATH
