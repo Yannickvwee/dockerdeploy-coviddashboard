8ffb5cfed27585ad05ff5b95d756fb8fbfe46114
Yannickvwee <yannickvanwee@gmail.com>
2020-10-29 13:30:33 +0100

    Update google-kubernetes-build.sh

--- google-kubernetes-build.sh
+++ google-kubernetes-build.sh
@@ -2,10 +2,10 @@
 #cd ./gitdir/nginx
 docker pull yannickvw/coviddashboard_nginx
 docker tag yannickvw/coviddashboard_nginx:latest eu.gcr.io/terraformingcloud/coviddashboard_nginx:latest
-docker build --tag eu.gcr.io/terraformingcloud/coviddashboard_nginx:latest /gitdir/nginx.
+docker build --tag eu.gcr.io/terraformingcloud/coviddashboard_nginx:latest /gitdir/nginx/.
 docker push eu.gcr.io/terraformingcloud/coviddashboard_nginx:latest
 #cd ../gitdir/netapp/master
 docker pull yannickvw/coviddashboard_netapp
 docker tag yannickvw/coviddashboard_netapp:latest eu.gcr.io/terraformingcloud/coviddashboard_nginx:netapp
-docker build --tag eu.gcr.io/terraformingcloud/coviddashboard_netapp:latest /gitdir/netapp/master.
+docker build --tag eu.gcr.io/terraformingcloud/coviddashboard_netapp:latest /gitdir/netapp/master/.
 docker push eu.gcr.io/terraformingcloud/coviddashboard_netapp:latest
