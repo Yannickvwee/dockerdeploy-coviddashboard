8cd84b53dae98cf982febde947ad9dc8474f2bac
Yannickvwee <yannickvanwee@gmail.com>
2020-10-29 13:24:06 +0100

    fixed google kub build script

--- google-kubernetes-build.sh
+++ google-kubernetes-build.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 #cd ./gitdir/nginx
+docker pull yannickvw/coviddashboard_nginx
 docker build --tag eu.gcr.io/terraformingcloud/coviddashboard_nginx:latest /gitdir/nginx.
 docker push eu.gcr.io/terraformingcloud/coviddashboard_nginx:latest
 #cd ../gitdir/netapp/master
+docker pull yannickvw/coviddashboard_netapp
 docker build --tag eu.gcr.io/terraformingcloud/coviddashboard_netapp:latest /gitdir/netapp/master.
 docker push eu.gcr.io/terraformingcloud/coviddashboard_netapp:latest
